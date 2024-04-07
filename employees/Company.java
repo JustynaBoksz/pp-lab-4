7202ed5c855f3647db0545e41e53727c74059cdc
JustynaBoksz <justynabokszczanin1@gmail.com>
2024-04-07 20:15:36 +0300

    zad3

--- employees/Company.java
+++ employees/Company.java
@@ -1,29 +1,32 @@
 package employees;
+
 public class Company {
 
-public static void main(String[] args) {
+  public static void main(String[] args) {
     Employee[] employees = new Employee[5];
 
-    employees[0] = new Manager("Jan Kowalski", 4000.0, 2); 
-    employees[1] = new Worker("Anna Nowak", 3500.0, "Sekretarka"); 
+    // Initial creation of employees (unchanged)
+    employees[0] = new Manager("Jan Kowalski", 4000.0, 2);
+    employees[1] = new Worker("Anna Nowak", 3500.0, "Sekretarka");
     employees[2] = new Worker("Tomasz Kot", 5000.0, "Programista");
     employees[3] = new Worker("Kasia Kropka", 4500.0, "Grafik");
-    employees[4] = new Worker("Bartek Mazurek", 2500.0, "Programista"); 
+    employees[4] = new Worker("Bartek Mazurek", 2500.0, "Programista");
 
-    ((Manager) employees[0]).setNumberOfSubordinates(2);
-
+    // Count workers and set as number of subordinates for the manager
     int numberOfWorkers = 0;
     for (Employee employee : employees) {
-        if (!(employee instanceof Manager)) {
-            numberOfWorkers++;
-        }
+      if (!(employee instanceof Manager)) {
+        numberOfWorkers++;
+      }
     }
     ((Manager) employees[0]).setNumberOfSubordinates(numberOfWorkers);
 
+    // Set salary of manager
     ((Manager) employees[0]).setSalary(7500.0);
 
+    // Print employee information (unchanged)
     for (Employee employee : employees) {
-        System.out.println(employee.toString());
+      System.out.println(employee.toString());
     }
-}
+  }
 }
