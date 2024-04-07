715ae874b1dd88b773cbbc7eaa2067d019260b63
JustynaBoksz <justynabokszczanin1@gmail.com>
2024-04-07 20:34:10 +0300

    zad2

--- employees/Company.java
+++ employees/Company.java
@@ -1,29 +1,29 @@
 package employees;
+
 public class Company {
 
-public static void main(String[] args) {
+  public static void main(String[] args) {
     Employee[] employees = new Employee[5];
 
-    employees[0] = new Manager("Jan Kowalski", 4000.0, 2); 
-    employees[1] = new Worker("Anna Nowak", 3500.0, "Sekretarka"); 
-    employees[2] = new Worker("Tomasz Kot", 5000.0, "Programista");
-    employees[3] = new Worker("Kasia Kropka", 4500.0, "Grafik");
-    employees[4] = new Worker("Bartek Mazurek", 2500.0, "Programista"); 
+    
+    employees[0] = new Manager("Jan Kowalski", 4000.0, 0);
 
-    ((Manager) employees[0]).setNumberOfSubordinates(2);
+    
+    employees[1] = new Worker("Anna Nowak", 3500.0, "Sekretarka");
+    employees[4] = new Worker("Bartek Mazurek", 2500.0, "Programista");
 
-    int numberOfWorkers = 0;
+    
+    employees[2] = new Employee("Tomasz Kot", 5000.0);
+    employees[3] = new Employee("Kasia Kropka", 4500.0);
+
+    System.out.println("Dane pracownika o indeksie 3:");
+    System.out.println(employees[3].toString());
+
+    employees[3].setSalary(4800.0);
+
+    System.out.println("Dane wszystkich pracowników:");
     for (Employee employee : employees) {
-        if (!(employee instanceof Manager)) {
-            numberOfWorkers++;
-        }
+      System.out.println(employee.toString());
     }
-    ((Manager) employees[0]).setNumberOfSubordinates(numberOfWorkers);
-
-    ((Manager) employees[0]).setSalary(7500.0);
-
-    for (Employee employee : employees) {
-        System.out.println(employee.toString());
-    }
-}
+  }
 }
